46aed15a4191e4ce60e7ac8fd0f8237d73ad2eba
Blanca Ubis <ubis.blanca@gmail.com>
2023-02-16 10:29:00 +0100

    Fix mistakes

--- js/main.js
+++ js/main.js
@@ -1,7 +1,13 @@
+'use strict';
+
+//Constantes
+const lastPalindrome = 9007199229917009;
+
 //Elementos HTML
-numberInput = document.querySelector('.js_input');
-button = document.querySelector('.js_button');
-result = document.querySelector('.js_result');
+const numberInput = document.querySelector('.js_input');
+const button = document.querySelector('.js_button');
+const result = document.querySelector('.js_result');
+const form = document.querySelector('.js_form');
 
 //Funciones auxiliares
 function isPalindrome(number) {
@@ -14,7 +20,7 @@ function isPalindrome(number) {
   return isPalindrome;
 }
 function nextPalindrome(number) {
-  nextNumber = number + 1;
+  let nextNumber = number + 1;
   while (!isPalindrome(nextNumber)) {
     nextNumber++;
   }
@@ -22,18 +28,27 @@ function nextPalindrome(number) {
 }
 
 //Funciones Handler
+function handleSubmit(ev) {
+  ev.preventDefault();
+}
 
 function handleClick() {
-  let number = 0;
-  if (numberInput.value < Number.MAX_SAFE_INTEGER) {
+  let number = null;
+  let nextPalindromeString = null;
+
+  if (numberInput.value >= 0 && numberInput.value < lastPalindrome) {
     number = parseInt(numberInput.value);
+    nextPalindromeString = nextPalindrome(number).toString();
+  } else {
+    result.innerHTML = 'Please enter a valid number';
   }
-  const nextPalindromeString = nextPalindrome(number).toString();
-  if (number) {
+
+  if (number >= 0) {
     result.innerHTML = `The next palindrome is ${nextPalindromeString}.`;
   } else {
     result.innerHTML = 'Please enter a valid number.';
   }
 }
 
+form.addEventListener('submit', handleSubmit);
 button.addEventListener('click', handleClick);
